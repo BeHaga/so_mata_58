f09a3bf551e26b743bd45bbab303c323b612881f
BeHaga <brunohenriqueluz5@gmail.com>
2025-02-04 17:48:37 -0300

    EDF 100% funcional

--- trabalho_principal_so/src/algoritmos/Eedf.tsx
+++ trabalho_principal_so/src/algoritmos/Eedf.tsx
@@ -1,148 +1,147 @@
-//versão +- funcional
-// tá executando 2 processos ao mesmo tempo 
-//e comendo "yellows" no caso teste com sobrecarga = 1
-
 function Eedf (processos: {key: number, tempoDeChegada: number, tempoDeExecucao: number, deadline: number, paginas: number}[], quantum: number, sobrecarga: number) {
     
-    const processosCopia: {key: number; tempoDeChegada: number; tempoDeExecucao: number; deadline: number; paginas: number; deadlineCheck: number; estadosProcesso: string[];}[] = [];
+    const processosCopia: {key: number; tempoDeChegada: number; tempoDeExecucao: number; deadline: number; paginas: number; deadlineRestante: number; tempoExecutado: number; estadosProcesso: string[];}[] = [];
+    const processosExecutados = [];
+    const matriz = [];
+    let fila: number[] = [];
+
     for (let i = 0; i < processos.length; i++) {
-        processosCopia.push({key: processos[i].key, tempoDeChegada: processos[i].tempoDeChegada, tempoDeExecucao: processos[i].tempoDeExecucao, deadline: processos[i].deadline, paginas: processos[i].paginas, 
-        deadlineCheck: ((processos[i].tempoDeChegada + processos[i].deadline)), estadosProcesso: []})
+        processosCopia.push({key: processos[i].key, tempoDeChegada: processos[i].tempoDeChegada, tempoDeExecucao: processos[i].tempoDeExecucao, deadline: processos[i].deadline, paginas: processos[i].paginas, deadlineRestante: processos[i].deadline, tempoExecutado: 0, estadosProcesso: []})
     }
-    
-    // usar esse vetor pra fila. Precisa ter os mesmos atributos da Cópia
-    const fila: {key: number; tempoDeChegada: number; tempoDeExecucao: number; deadline: number; paginas: number; deadlineCheck: number; estadosProcesso: string[];}[] = [];
-    //adicionar aqui os processos finalizados
-    const processoTerminou: {key: number; tempoDeChegada: number; tempoDeExecucao: number; deadline: number; paginas: number; deadlineCheck: number; estadosProcesso: string[];}[] = [];
 
-    const processosExecutados = []; // eixo y do gráfico
-    const matriz = []; // gráfico completo
+    function isFila (i: number) {
+        if (fila.includes(i)) {
 
-    
+        } else {
+            fila.push(i)
+        }
+    }
+
+    function ordenarPorDeadlineRestante () {
+        const filaNova: number[] = [];
+        let aux = fila.length
+        while (aux != 0) {
+            let menorDealine = processosCopia[fila[0]].deadlineRestante
+            let value = fila[0] //0
+            for (let i = 1; i < fila.length; i++) {
+                if (menorDealine > processosCopia[fila[i]].deadlineRestante) {
+                    menorDealine = processosCopia[fila[i]].deadlineRestante
+                    value = fila[i]
+                }
+            }
+            filaNova.push(value) //push 0
+            fila.splice(fila.indexOf(value), 1) //remove da 1ª lista
+            aux -= 1
+        }
+        fila = filaNova
+    }
+
     let tempoDecorrido = 0; //exe + nda + sobrecarga || determina se algum processo já foi executado
-    let tempoGasto = 0; //exe + esp + sobrecarga || usar pra calcular turnaround médio
+    let tempoGasto = 0; //exe + esp + sobrecarga
+
     // let aux = 0; //exe + esp + sobrecarga + quantum
-    // let qtdGreen = 0
-    // let qtdRed = 0
+    let qtdGreen = 0
+    let qtdRed = 0
 
-    Escalonar()
-    function Escalonar() {
-      while (processosCopia.length > 0 || fila.length > 0) {
-        //1. Adicionar os que chegaram na fila
-        for (let i = processosCopia.length - 1; i >=0; i--) {
-          if (processosCopia[i].tempoDeChegada <= tempoDecorrido) {
-            fila.push(processosCopia[i])
-            processosCopia.splice(i, 1)
-          }
-        }
+    // uso ela caso alguma variavel chegue durante a execucao do red
+    let sobrecargaAcabou: number[] = [];
 
-        // se a fila estiver vazia, só avança o tempoDecorrido
-        // se não estiver,  esse bloco ñ executa
-        // fazer o tempoDecorrido avançar nas outras condições
-        if (fila.length === 0) {
-            tempoDecorrido++
-            console.log("tempo decorrido:" + tempoDecorrido)
-            continue
-        }
+    let processoAcabou: number[] = [];
 
-        //escolher o menor deadline pra processar na marra com um loop manual
-        // let processoSelecionado = fila[0];
-        //     for (let i = 1; i < fila.length; i++) {
-        //         if (fila[i].deadline < processoSelecionado.deadline) {
-        //             processoSelecionado = fila[i];
-        //         }
-        //     }
-
-        //alt pra pegar o menor, dando sort e pegando o primeiro elemento
-        fila.sort((a, b) => a.deadlineCheck - b.deadlineCheck)
-
-
-        let tempoExeRestante = Math.min(fila[0].tempoDeExecucao, quantum)
-        console.log("ANTES DE PROCESSAR:" + tempoExeRestante)
-
-        for (let i = 0; i < tempoExeRestante; i++) {
-            if (tempoDecorrido > fila[0].deadlineCheck) {
-                fila[0].estadosProcesso.push("black")
-
-                //loop pra adicionar "yellow nos outros processos"
-                for (let j = 1; j < fila.length; j++) {
-                    console.log("deu black. subindo yellow para o processo " + fila[j].key)
-                    fila[j].estadosProcesso.push("yellow")
+    Executar()
+    function Executar () {
+        for (let i = 0; i < processosCopia.length; i++) {
+            if (processosCopia[i].tempoDeChegada == tempoDecorrido) {
+                isFila(i)
+            }
+            //o processo está esperando
+            if ((fila.includes(i)) && (i != fila[0])) {
+                // console.log("Sou o processo: " + processosCopia[i].key + " e estou esperando")
+                // aux += 1
+                tempoGasto += 1
+                processosCopia[i].estadosProcesso.push("yellow")
+                processosCopia[i].deadlineRestante -= 1
+            //o processo executou
+            } else if (i == fila[0]) {
+                //se não estourou o quantum
+                if ((qtdGreen < quantum)) {
+                    // console.log("Sou o processo: " + processosCopia[i].key + " e estou executando")
+                    tempoGasto += 1
+                    // tempoDecorrido += 1
+                    processosCopia[i].tempoExecutado += 1
+                    if (processosCopia[i].deadlineRestante > 0) {
+                        processosCopia[i].estadosProcesso.push("green")
+                    } else {
+                        processosCopia[i].estadosProcesso.push("black")
+                    }
+                    qtdGreen += 1
+                    processosCopia[i].deadlineRestante -= 1
+                //se estourou o quanto
+                } else if ((qtdGreen == quantum) && ((processosCopia[i].tempoDeExecucao - processosCopia[i].tempoExecutado) > 0) && (qtdRed < sobrecarga)) {
+                    processosCopia[i].estadosProcesso.push("red")
+                    tempoGasto += 1
+                    // tempoDecorrido += 1
+                    qtdRed += 1
+                    processosCopia[i].deadlineRestante -= 1
+                    // console.log("Sou o processo: " + processosCopia[i].key + " e estou em sobrecarga")
                 }
-                fila[0].tempoDeExecucao--
-                console.log("tempo do processo" + fila[0].tempoDeExecucao)
-                tempoDecorrido++;
-                tempoGasto++;
-                console.log("tempo decorrido processo " + fila[0].key  + " é: " + tempoDecorrido)
-                console.log("tempo gatsto:" + tempoGasto)
+                //se já imprimiu todos os reds
+                if (qtdRed == sobrecarga) {
+                    qtdGreen = 0
+                    qtdRed = 0
+                    sobrecargaAcabou.push(i)
+                }
+                //se já acabou de executar completamente
+                if (processosCopia[i].tempoDeExecucao == processosCopia[i].tempoExecutado) {                        
+                    qtdGreen = 0
+                    processoAcabou.push(i)
+                }
+            //processo não chegou
+            } else if (processosCopia[i].tempoDeChegada > tempoDecorrido) {
+                // console.log("Sou o processo: " + processosCopia[i].key + " e ainda não cheguei")
+                // aux += 1
+                processosCopia[i].estadosProcesso.push("gray")
+            //o processo já executou tudo               
             } else {
-                fila[0].estadosProcesso.push("green")
-
-                //loop pra adicionar "yellow nos outros processos"
-                for (let j = 1; j < fila.length; j++) {
-                    fila[j].estadosProcesso.push("yellow")
-                }
-                fila[0].tempoDeExecucao--
-                tempoDecorrido++;
-                tempoGasto++;
-                console.log("tempo decorrido:" + tempoDecorrido)
-                console.log("tempo gatsto:" + tempoGasto)
-            }
+                // console.log("Sou o processo: " + processosCopia[i].key + " e já executei tudo")
+                processosCopia[i].estadosProcesso.push("gray")
+            }                                    
         }
-
-        tempoExeRestante = 0
-
-        //contabilizar sobrecarga || salvar processo finalizado
-        //se tiver tempo restante, volta pra fila
-        if (fila[0].tempoDeExecucao > 0) {
-            for(let i = 0; i < sobrecarga; i++) {
-                fila[0].estadosProcesso.push("red")
-                console.log("deu red: " + fila[0].tempoDeExecucao)
-
-                //loop pra adicionar "yellow nos outros processos"
-                for (let j = 1; j < fila.length; j++) {
-                    fila[j].estadosProcesso.push("yellow")
-                }
-                tempoDecorrido++;
-                tempoGasto++;
-                console.log("tempo decorrido:" + tempoDecorrido)
-                console.log("tempo gatsto:" + tempoGasto)
-            }
-
-            processosCopia.push(fila[0])
-            fila.splice(0, 1)
-
-        } else { //se já terminou, é adicionado ao vetor de processos finalizados
-            processoTerminou.push(fila[0])
-            fila.splice(0, 1)
+        if (sobrecargaAcabou.length != 0) {                   
+            // fila.push(sobrecargaAcabou[0])
+            // fila.shift()
+            sobrecargaAcabou.shift()
         }
-
-      }
+        if (processoAcabou.length != 0) {
+            fila.shift()
+            processoAcabou.shift()
+        }
+        tempoDecorrido += 1        
+        ordenarPorDeadlineRestante()
     }
 
-    let tempoMedio = (tempoGasto)/(processoTerminou.length)
-
-    //adicionar "gray" pra simbolizar o tempo antes de cada processo chegar
-    processoTerminou.sort((a, b) => a.key - b.key)
-    for (let i = 0; i < processoTerminou.length; i++) {
-        for (let j = 0; j < processoTerminou[i].tempoDeChegada; j++){
-            processoTerminou[i].estadosProcesso.unshift("gray")
-        }
+    while (processosCopia.some(p => p.tempoExecutado != p.tempoDeExecucao)) {
+        Executar()
     }
+    // for (let i = 0; i < 68; i++) {
+    //     Executar()
+    // }
+    
+    let tempoMedio = (tempoGasto)/(processosCopia.length)
 
     //add estados na matriz
-    for (let i = 0; i < processoTerminou.length; i++) {
-        matriz.push(processoTerminou[i].estadosProcesso)
-        processosExecutados.push(processoTerminou[i].key)
+    for (let i = 0; i < processosCopia.length; i++) {
+        matriz.push(processosCopia[i].estadosProcesso)
+        processosExecutados.push(processosCopia[i].key)
     }
 
     //creio que isso não ocorra mais
-    for (let i = 0; i < matriz.length; i++) {
-        while(matriz[i].length < tempoDecorrido) { //antes era tempoDecorrido + 1
-            // matriz[i].push("nda")
-            matriz[i].push("gray")
-        }
-    }
+    // for (let i = 0; i < matriz.length; i++) {
+    //     while(matriz[i].length < tempoDecorrido) { //antes era tempoDecorrido + 1
+    //         // matriz[i].push("nda")
+    //         matriz[i].push("gray")
+    //     }
+    // }
 
     const eixox = []
 
@@ -158,7 +157,7 @@ function Eedf (processos: {key: number, tempoDeChegada: number, tempoDeExecucao:
 }
 
 //para base de teste
-//tempo médio esperado: 23
+//tempo médio esperado: 49
 const processosTeste = [
     { key: 1, tempoDeChegada: 0, tempoDeExecucao: 14, deadline: 10, paginas: 1 },
     { key: 2, tempoDeChegada: 2, tempoDeExecucao: 4, deadline: 12, paginas: 1 },
@@ -166,7 +165,8 @@ const processosTeste = [
     { key: 4, tempoDeChegada: 6, tempoDeExecucao: 6, deadline: 15, paginas: 1 },
 ]
 
-// console.log(Eedf(processosTeste, 2, 1))
+// console.log(Eedf(processosTeste, 1, 2).tempoMedio)
+// console.log(Eedf(processosTeste, 1, 2).matriz)
 // console.log(Eedf(processos1, 2, 1))
 
 export default Eedf
